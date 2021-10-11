2e37410c55ad101872a76ca67cfbb02089d0e93b
Levchuk Elena <vepevo4ka@gmail.com>
2021-10-12 02:56:09 +0300

    task1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,19 +1,22 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
-
+    
     // Задание №1.1. Оповещение о добавлении в корзину
 
-    // создайте переменную message
+     // создайте переменную message
     // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
+     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'за'
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message значение переменной productPrice
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
-
+    
+    let message = productName;
+    console.log(message+' '+'за'+' '+productPrice+' '+'теперь в корзине!');
+  
     // Конец решения задания №1.1.
 
     return message;
@@ -22,12 +25,16 @@ function addInCartMessage(title, price) {
 function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
+    
     let oldValue = value;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
 
+    let newValue = oldValue;
+    console.log (newValue+1);
+    
     // Конец решения задания №1.2.
 
     return newValue;
@@ -42,6 +49,9 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
+
+    let newSum = newSum+difference;
+    let newSumText =`${newSum} ₽`;
 
     // Конец решения задания №1.3.
 
